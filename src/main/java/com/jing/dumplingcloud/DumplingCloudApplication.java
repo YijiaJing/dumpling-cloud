11ed848e848c07fc5b55a92927f406cf700e9c49
YijiaJing <47167363+YijiaJing@users.noreply.github.com>
2021-05-01 17:06:48 +0800

    Added initial Spring template

---
+++ src/main/java/com/jing/dumplingcloud/DumplingCloudApplication.java
@@ -0,0 +1,14 @@
+package com.jing.dumplingcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DumplingCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DumplingCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jing/dumplingcloud/DumplingCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jing.dumplingcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumplingCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
